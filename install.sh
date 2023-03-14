38a2cff8661679349d1cf6677d6d8bf47e0c7b50
edwardOWO <a0981202524@gmail.com>
2023-03-14 06:13:02 +0000

    update acme

---
+++ install.sh
@@ -0,0 +1,2 @@
+curl https://get.acme.sh | sh -s email=test@gmail.com --install --force
+cp /root/.acme.sh/acme.sh /usr/local/bin/acme.sh
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+type challenge_token struct {
+	challenge_domain string
+	challenge_txt    string
+}
+
+// test for acme.sh
+func main() {
+	fmt.Println("!... Hello World ...!")
+
+	setting_acme_server("letsencrypt")
+
+	output := issue_ca("letsencrypt", "test.ltpix.link")
+
+	issue_challenge_token := getChallenge(&output)
+
+	output2 := renew_ca("letsencrypt", "test.ltpix.link")
+	renew_challenge_token := getChallenge(&output2)
+
+	fmt.Println(issue_challenge_token.challenge_domain)
+	fmt.Println(issue_challenge_token.challenge_txt)
+
+	fmt.Println(renew_challenge_token.challenge_domain)
+	fmt.Println(renew_challenge_token.challenge_txt)
+
+}
+func setting_acme_server(server string) error {
+
+	command := "acme.sh --set-default-ca --server " + server
+
+	result, err := exec.Command("bash", "-c", command).Output()
+
+	if err != nil {
+		fmt.Print(err.Error())
+	} else {
+		fmt.Print(string(result))
+	}
+
+	return err
+}
+
+func issue_ca(server string, domain string) []string {
+
+	command := fmt.Sprintln("acme.sh --issue -d test.ltpix.link --server letsencrypt --dns --yes-I-know-dns-manual-mode-enough-go-ahead-please --log")
+
+	result, _ := exec.Command("bash", "-c", command).Output()
+
+	data := strings.Split(string(result), "\n")
+
+	return data
+}
+
+func renew_ca(server string, domain string) []string {
+
+	command := fmt.Sprintln("acme.sh --issue -d test.ltpix.link --renew --server letsencrypt --dns --yes-I-know-dns-manual-mode-enough-go-ahead-please --log")
+
+	result, _ := exec.Command("bash", "-c", command).Output()
+
+	data := strings.Split(string(result), "\n")
+
+	return data
+
+}
+func getChallenge(result *[]string) challenge_token {
+	regex := regexp.MustCompile(`_acme-challenge\.[A-Za-z0-9]+\.[A-Za-z0-9]+`)
+
+	regex2 := regexp.MustCompile(`'([A-Za-z0-9]+)'$`)
+
+	var txtValue string
+	var challenge_domain string
+	// 使用正規表達式擷取完整的子域名
+	for _, data := range *result {
+
+		if len(regex.FindString(data)) > 1 {
+			challenge_domain = regex.FindString(data)
+		}
+
+		matches := regex2.FindStringSubmatch(data)
+
+		if len(matches) > 1 {
+			txtValue = matches[1]
+			fmt.Println(txtValue)
+		}
+	}
+
+	return challenge_token{challenge_domain, txtValue}
+
+}
